814cb586c4b39fa41cdb83160dd2382b904fa686
karinapikulenko <karina.pikulenko23@gmail.com>
2025-11-16 14:35:29 +0300

    Building a simple string inverter
    A build string inverter has been uploaded

---
+++ build-a-string-inverter.js
@@ -0,0 +1,11 @@
+** start of script.js **
+
+function reverseString(str){
+  return str.split("").reverse().join("");
+}
+console.log(reverseString("hello"));
+console.log(reverseString("Howdy"));
+console.log(reverseString("Greeting from Earth"));
+
+** end of script.js **
+
